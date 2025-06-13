473aa6a59e190e66aad1c166980451ec583ccf48
harsh <harsh006228@gmail.com>
2025-06-14 00:44:00 +0530

    Delete valid.py

--- valid.py
+++
@@ -1,5 +0,0 @@
-import json
-
-data = {"question": "Should I use gpt-4o-mini which AI proxy supports, or gpt3.5 turbo?", "image": "$(base64 -w0 project-tds-virtual-ta-q1.webp)"}
-json_data = json.dumps(data)
-print(json_data)
